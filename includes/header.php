9eab0ce54c2e5a31c876f03b8ad4aa2185b1ea77
takanashataryu <tatsuarieyu@gmail.com>
2025-05-16 20:16:15 +0700

    modified:   includes/header.php

--- includes/header.php
+++ includes/header.php
@@ -19,7 +19,8 @@ if (isset($_SESSION['alert'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Krauk-Krauk - Pemesanan Makanan Online</title>
-    <script src="https://cdn.tailwindcss.com"></script>
+
+    <link rel="stylesheet" href="   assets\css\styles.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <link rel="icon" type="image/x-icon" href="../assets/images/favicon.ico">
